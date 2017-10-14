a0a5b16a22f8f914c3f0a7fb3dad70c235e98ab9
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-10-14 09:09:45 -0500

    modificando

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+                    // $('.a-menu__item__link').animate({
+                    //            'margin-left':'600%'
+                    // },400);
+ 
+                    $('.a-menu__item').addClass("animated zoomIn");
+ 
+
+          });
